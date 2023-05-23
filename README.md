aceab16193d2af61dda909f37a3db667e47317d5
Juan Negro <jnegro@itba.edu.ar>
2023-05-23 18:32:27 -0300

    Add basic main

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tpe-redes
+
+## Authors
+
+- Juan Martín Barmasch <jbarmasch@itba.edu.ar>
+- Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
+- Juan Negro <jnegro@itba.edu.ar>
+
+## Requirements
+
+-
--- src/main.py
+++ src/main.py
@@ -8,11 +8,16 @@ load_dotenv()
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": getenv("POD_MSG", "Hello World!")}
+
+
 if __name__ == "__main__":
-    port = getenv("FASTAPI_PORT", 8080)
+    port = getenv("FASTAPI_PORT", 8000)
     try:
         port = int(port)
     except ValueError:
-        port = 8080
+        port = 8000
 
     uvicorn.run(app, host="0.0.0.0", port=port)
